e022ea1aca2ce436951257468edf8f82146d592c
Gustavo Ferrari <gustavohz99@gmail.com>
2023-01-30 16:18:09 -0300

    Corzinha que funciona e um Save State que talvez funciona

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 
 import re
 from difflib import SequenceMatcher
-import time
-import fileinput
+
+class bcolors:
+    OKBLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
 
 if __name__ == '__main__':
     indexnum = 0
@@ -29,7 +33,7 @@ if __name__ == '__main__':
                 maybetranslation = ""
                 wordbest = ""
                 iterations = 0
-                for j in range (indexnum-5, num_lines):
+                for j in range (indexnum-50, num_lines):
                     s = SequenceMatcher(None, texto[i].replace('\n', ''), scriptlinesEN[j].replace('\n', ''))
                     #print(texto[i] + scriptlinesEN[j] + str(s.ratio()))
                     if s.ratio() > maybe:
@@ -42,18 +46,25 @@ if __name__ == '__main__':
                         cond = "aeooo"
                         passed = "yes"
                         print(mudado)
-                    if iterations > 50:
+                    if iterations > 100:
                         break
                     iterations +=1
                 if passed == None:
-                    print("Original: " + texto[i])
-                    print("Script EN: " + maybebest)
-                    print("Script PT: " + maybetranslation)
+                    print(bcolors.RED + "Original: " + texto[i] + "                index: " + str(indexnum+1) + bcolors.ENDC)
+                    print(bcolors.YELLOW + "Script EN: " + maybebest)
+                    print("Script PT: " + maybetranslation + bcolors.ENDC)
+                    print(bcolors.OKBLUE + "Script EN 2: " + scriptlinesEN[indexnum])
+                    print("Script PT 2: " + scriptlines[indexnum] + bcolors.ENDC)
+
                     manual = input("Frase: ")
                     if manual == "y":
                         mudado = mudado.replace(texto[i], maybetranslation.replace('\n', ''))
+                    elif manual == "y2":
+                        mudado = mudado.replace(texto[i], scriptlines[indexnum].replace('\n', ''))
                     elif manual == "n":
                         pass
+                    elif manual == "n2":
+                        indexnum -= 1
                     else:
                         mudado = mudado.replace(texto[i], manual.replace('\n', ''))
                     cond = "aeooo"
@@ -67,13 +78,31 @@ if __name__ == '__main__':
 
 
 
-    f = open(r"C:\Users\gusta\Desktop\teste.u", "r", encoding="utf-8")
+    f = open("original.u", "r", encoding="utf-8")
     lines = f.readlines()
-    g = open("myfile.u", "w", encoding="utf-8")
+    savedfile = None
+
+    nf = input("Create a new file?  y = yes")
+    if nf == "y":
+        g = open("myfile.u", "w", encoding="utf-8")
+    else:
+        g = open("myfile.u", "r+", encoding="utf-8")
+        savedfile = g.readlines()
+
     script = open("script.txt", "r", encoding="utf-8")
     scriptlines = script.readlines()
     scriptEN = open("scripten.txt", "r", encoding="utf-8")
     scriptlinesEN = scriptEN.readlines()
+
+
     num_lines = sum(1 for line in scriptlinesEN)
+    savestateline = sum(1 for line in savedfile)
+    print(savestateline)
+    savestateindex = 319
+    indexnum = savestateindex
+    countline = 0
     for line in lines:
-        testefun(line)
+        if countline == savestateline:
+            testefun(line)
+        else:
+            countline += 1
